1de7afb028673b9cbfaabb891db9becf2279128f
Scott Lyons <scottalyons@gmail.com>
2016-02-29 10:57:25 -0800

    Adding BDD testing to slacker bot

---
+++ Slacker.tests/SlackerStepDefinitions.fs
@@ -0,0 +1,20 @@
+﻿module SlackerStepDefinitions
+
+open System
+open Slacker.API
+open TickSpec
+open NUnit.Framework
+
+let mutable command = ["none"; "none"]
+
+let [<Given>] ``a slacker bot`` () = 
+    ()
+
+let [<When>] ``I issue command "(.*)"`` (com:string)=
+    command <- com.Split(' ') |> Array.toList
+
+let [<Then>] ``it should reply "(.*)"`` (expected:string) =
+    let response = ProcessRequest command
+    match response with
+    | PublicResponse(s) -> Assert.AreEqual(expected, s)
+    | _ -> Assert.Fail()
